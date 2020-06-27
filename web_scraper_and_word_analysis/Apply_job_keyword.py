b75882c5a86b63dfb01d7b83da7cd697468c202d
kollerbud <kollerbud@gmail.com>
2020-06-27 16:11:11 -0400

    v_1

---
+++ web_scraper_and_word_analysis/Apply_job_keyword.py
@@ -0,0 +1,10 @@
+import requests as req
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+import random
+from nltk import word_tokenize, Text, FreqDist, pos_tag, ngrams
+from nltk.corpus import stopwords
+
+
+
